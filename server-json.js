71a7936f4377a20128e0cbb387fdc3de913e79d7
fkoussa <firaskoussa@gmail.com>
2023-01-21 18:34:40 +0400

    Background CSS + Bootstrap Cards

--- server-json.js
+++ server-json.js
@@ -1,16 +1,17 @@
 const cors = require("cors");
-
 const express = require("express");
+
 const port = 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
+
 const courses = [
-  { topic: "math", location: "hendon", price: 100 },
-  { topic: "math", location: "colindale", price: 80 },
-  { topic: "math", location: "brent cross", price: 90 },
-  { topic: "math", location: "golders green", price: 120 },
+  { topic: "Math", location: "Hendon", price: 100 },
+  { topic: "Math", location: "Colindale", price: 80 },
+  { topic: "Math", location: "Brent Cross", price: 90 },
+  { topic: "Math", location: "Golders Green", price: 120 },
 ];
 
 const user = [ {email: "jomama@email.com", password: "pass"}]
